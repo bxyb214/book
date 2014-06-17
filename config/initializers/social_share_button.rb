d764f29ba51070d8253c37a0662e927411ee2cf9
zhaiyingchang <369722924@qq.com>
2014-06-17 18:40:46 +0800

    a little fix about social buttons

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo douban tqq renren qq )
+  config.allow_sites = %w(weibo douban tqq renren qq twitter facebook google_plus)
 end
