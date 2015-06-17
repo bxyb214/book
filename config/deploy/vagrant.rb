408858edd0d1561ca17ed665848c4fd553262ffd
JeremyXu <jemeryxu@gmail.com>
2015-06-17 14:58:34 +0800

    deploy 2

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,4 +1,4 @@
-server '192.168.33.10', user: 'vagrant', roles: %w{web app db}
+server '192.168.33.10', user: 'postgres', roles: %w{web app db}
 
 set :ssh_options, {
   keys: '~/.vagrant.d/insecure_private_key'
